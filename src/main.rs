bf0e26ff25adfe750562ec1cab4dacbb4e5ee564
LispreTxd <lispre@outlook.com>
2018-01-18 18:03:39 +0800

    add type support and change output order

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ use colored::*;
 
 use std::env::args;
 use std::io;
-use std::fs::File;
+use std::fs::{File, DirEntry};
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::io::prelude::*;
+use std::ffi::OsString;
+use std::collections::VecDeque;
 
 fn is_text(file_path: &Path) -> bool {
     if let Ok(mut file_handle) = File::open(file_path) {
@@ -50,7 +52,6 @@ fn is_text(file_path: &Path) -> bool {
     } else {
         false
     }
-
 }
 
 fn call_back(de: &Path, pt: &String) {
@@ -75,7 +76,7 @@ fn call_back(de: &Path, pt: &String) {
                             let v: Vec<&str> = ln.as_str().split(pt).collect();
                             let v_len = v.len();
                             print!("{}:", line_num);
-                            for i in 1..v_len+1 { 
+                            for i in 1..v_len+1 {
                                 if i == v_len {
                                     println!("{}", &v[i-1]);
                                 } else {
@@ -94,32 +95,206 @@ fn call_back(de: &Path, pt: &String) {
     }
 }
 
+//fn main() {
+//    let args: Vec<String> = args().collect();
+//    if args.len() != 3 && args.len() != 5 {
+//	      println!("usage: sf pattern-string root-directory");
+//        println!("       sf -t file_ext pattern-string root-directory");
+//        println!("       sf pattern-string root-directory -t file_ext");
+//        return;
+//    }
+//
+//    if args.len() == 3 {
+//	      let pattern_str = &args[1];
+//        let root_dir = &args[2];
+//
+//        let pt = Path::new(&root_dir);
+//        walk_through_dir(&pt, &"".to_string(), &pattern_str, &call_back);
+//    } else {
+//        if args.contains(&"-t".to_string()) {
+//            if let Some(index) = args.iter().position(|&ref r| *r == "-t".to_string()) {
+//                if index == 1 {
+//                    let pattern_str = &args[3];
+//                    let root_dir = &args[4];
+//                    let pt = Path::new(&root_dir);
+//                    walk_through_dir(&pt, &args[2], &pattern_str, &call_back);
+//                } else if index == 3 {
+//                    let pattern_str = &args[1];
+//                    let root_dir = &args[2];
+//                    let pt = Path::new(&root_dir);
+//                    walk_through_dir(&pt, &args[4], &pattern_str, &call_back);
+//                } else {
+//                    
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//fn walk_through_dir(dir: &Path,
+//                    file_ext: &String,
+//                    pattern_str: &String,
+//                    cb: &Fn(&Path, &String)) -> io::Result<()>
+//{
+//    // if dir.is_dir() {
+//    //     for entry in fs::read_dir(dir)? {
+//    //         let entry = entry?;
+//    //         let path = entry.path();
+//    //         walk_through_dir(&path, file_ext, pattern_str, cb);
+//    //     }
+//    // } else {
+//    //     if *file_ext == "".to_string() {
+//    //         cb(&dir, pattern_str);
+//    //     } else {
+//    //         if let Some(ext_name) = dir.extension() {
+//    //             if OsString::from(file_ext) == ext_name {
+//    //                 cb(&dir, pattern_str);
+//    //             } else {
+//    //             }
+//    //         } else {
+//    //         }
+//    //     }
+//    // }
+//    let mut common_files: Vec<Path> = vec![];
+//    let mut dirs: Vec<Path> = vec![];
+//
+//    // is directory
+//    if dir.is_dir() {
+//        for entry in fs::read_dir(dir)? {
+//            let entry_ = entry?;
+//            if !entry_.is_dir() {
+//                common_files.append(entry_);
+//            } else {
+//                dires.append(entry_);
+//            }
+//        }
+//    } else { // is common file
+//        if *file_ext == "".to_string() {
+//            cb(element, pattern_str);
+//        } else {
+//            if let Some(ext_name) = dir.extension() {
+//                if OsString::from(file_ext) == ext_name {
+//                    cb(&dir, pattern_str);
+//                } else {
+//                }
+//            } else {
+//
+//            }
+//    }
+//
+//    Ok(())
+//}
+
 fn main() {
     let args: Vec<String> = args().collect();
-    if args.len() != 3 {
-        println!("usage: sf pattern-string root-directory");
+    if args.len() != 3 && args.len() != 5 {
+    	println!("usage: sf pattern-string root-directory");
+        println!("       sf -t file_ext pattern-string root-directory");
+        println!("       sf pattern-string root-directory -t file_ext");
+        println!("eg:    sf main ./src");
+        println!("eg:    sf -t cpp main ./src");
+        println!("eg:    sf main ./src -t cpp");
         return;
     }
-    let pattern_str = &args[1];
-    let root_dir = &args[2];
 
-    let pt = Path::new(&root_dir);
-    walk_through_dir(&pt, &pattern_str, &call_back);
-}
+    let mut queue: VecDeque<PathBuf> = VecDeque::new();
+    
+    if args.len() == 3 {
+	    let pattern_str = &args[1];
+        let root_dir = &args[2];
 
-fn walk_through_dir(dir: &Path,
-                    pattern_str: &String,
-                    cb: &Fn(&Path, &String)) -> io::Result<()> 
-{
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            walk_through_dir(&path, pattern_str, cb)?;
+        let pt = Path::new(&root_dir);
+        let v = find_match(pt, &"".to_string(), pattern_str, &call_back);
+        for element in v {
+            queue.push_back(element);
+        }
+        while queue.len() != 0 {
+            match queue.pop_front() {
+                Some(path_buf) => {
+                    for element in find_match(path_buf.as_path(), &"".to_string(), pattern_str, &call_back) {
+                        queue.push_back(element);
+                    }
+                }
+                None => ()
+            }
         }
     } else {
-        cb(&dir, pattern_str);
+        if args.contains(&"-t".to_string()) {
+            if let Some(index) = args.iter().position(|&ref r| *r == "-t".to_string()) {
+                if index == 1 {
+                    let pattern_str = &args[3];
+                    let root_dir = &args[4];
+                    let pt = Path::new(&root_dir);
+                    let v = find_match(pt, &args[2], pattern_str, &call_back);
+                    for element in v {
+                        queue.push_back(element);
+                    }
+                    while queue.len() != 0 {
+                        match queue.pop_front() {
+                            Some(path_buf) => {
+                                for element in find_match(path_buf.as_path(), &args[2], pattern_str, &call_back) {
+                                    queue.push_back(element);
+                                }
+                            }
+                            None => ()
+                        }
+                    }
+                } else if index == 3 {
+                    let pattern_str = &args[1];
+                    let root_dir = &args[2];
+                    let pt = Path::new(&root_dir);
+                    let v = find_match(pt, &args[4], pattern_str, &call_back);
+                    for element in v {
+                        queue.push_back(element);
+                    }
+                    while queue.len() != 0 {
+                        match queue.pop_front() {
+                            Some(path_buf) => {
+                                for element in find_match(path_buf.as_path(), &args[4], pattern_str, &call_back) {
+                                    queue.push_back(element);
+                                }
+                            }
+                            None => ()
+                        }
+                    }
+                } else {
+                    
+                }
+            }
+        }
     }
+}
 
-    Ok(())
+
+fn find_match(root_dir: &Path, file_ext: &String, pattern_str: &String, cb: &Fn(&Path, &String)) -> Vec<PathBuf> {
+    let mut vec: Vec<PathBuf> = vec![];
+
+    match fs::read_dir(root_dir) {
+        Ok(iterator_obj) => {
+            for entry in iterator_obj {
+                match entry {
+                    Ok(ref dir_entry) => {
+                        if !dir_entry.path().as_path().is_dir() {
+                            if *file_ext == "".to_string() {
+                                cb(dir_entry.path().as_path(), pattern_str);
+                            } else {
+                                if let Some(ext_name) = dir_entry.path().as_path().extension() {
+                                    if OsString::from(file_ext) == ext_name {
+                                        cb(dir_entry.path().as_path(), pattern_str);
+                                    } else {
+                                    }
+                                } else {
+                                }
+                            }
+                        } else {
+                            vec.push(dir_entry.path());
+                        }
+                    }
+                    Err(err_obj) => ()
+                }
+            }
+        }
+        Err(error_obj) => ()
+    }
+    vec
 }
